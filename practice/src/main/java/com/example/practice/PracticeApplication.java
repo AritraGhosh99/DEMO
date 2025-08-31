e29423263b7b7897d68a1f09dd4baeae4cc71466
GHOSH <aritra.a.ghosh@capgemini.com>
2025-08-31 17:22:35 +0530

    Aritra's second commit

--- practice/src/main/java/com/example/practice/PracticeApplication.java
+++ practice/src/main/java/com/example/practice/PracticeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticeApplication {
 
 	public static void main(String[] args) {
-
+		int a=1;
 		SpringApplication.run(PracticeApplication.class, args);
 	}
 
