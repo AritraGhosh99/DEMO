65e5d729574161b81eb12f320b0e460a7b126009
GHOSH <aritra.a.ghosh@capgemini.com>
2025-08-31 17:46:59 +0530

    Aritras third changes

--- practice/src/main/java/com/example/practice/PracticeApplication.java
+++ practice/src/main/java/com/example/practice/PracticeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracticeApplication {
 
 	public static void main(String[] args) {
-		int a=1;
+		int b=1;
 		SpringApplication.run(PracticeApplication.class, args);
 	}
 
